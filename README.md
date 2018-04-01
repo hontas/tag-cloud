d7eb7166530f2ab8bf610327221c6a94ecb046e1
hontas <lundin.pontus@gmail.com>
2018-04-01 23:32:13 +0200

    add eslint node security plugin, update readme, validate query

--- README.md
+++ README.md
@@ -15,10 +15,15 @@ npm run dev
 ## TODO
 
 * A whole bunch of stuff
-* Testing (not too much, mostly integration)
+* Testing - not too much, mostly integration ;)
 * Break out components
 * Use stylus + CSS modules
 * Manage style related variables
 * ServiceWorker
 * Improve error handling, fetchWrapper, server
+* Add logger
 * Calculate tags in child process
+* Use forever or PM2
+
+https://expressjs.com/en/advanced/best-practice-security.html
+https://expressjs.com/en/advanced/best-practice-performance.html
--- server/routes/twitter/controller.js
+++ server/routes/twitter/controller.js
@@ -1,14 +1,18 @@
 const twitterApi = require('./api');
 const cleanTweet = require('../../utils/cleanTweet');
 const getTagsFromText = require('../../../common/getTagsFromText');
+const { isHashTag } = require('../../../common/validators');
 
 // TODO: calculate tags in childprocess / worker
 // TODO: support stop words from user
 const stopWords = ['the', 'and', 'for', 'that', 'att', 'för', 'och'];
 
 function get(req, res) {
+  const query = req.query.q;
+  if (!isHashTag(query)) return res.status(400).json({ message: 'Query must be a valid hashtag' });
+
   return twitterApi
-    .search(req.query.q)
+    .search(query)
     .then((tweets) => tweets.map(cleanTweet).join(' '))
     .then((text) => getTagsFromText(text, { stopWords }))
     .then((tags) => res.json(tags.slice(0, 100)))
